1a3a0be27819ed540b561a9eeffc7ae34d032c78
Maryam Mirzada <marimmirzada@gmail.com>
2026-02-14 21:02:13 +0430

    created the SearchBar componenet

--- country-explorer/src/App.jsx
+++ country-explorer/src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Globe } from "lucide-react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div>
-     
+      <div className="body d-flex justify-content-center align-items-center pt-5 gap-2">
+        <span>
+          <Globe
+            size={20}
+            style={{ color: "Orange" }}
+            className="align-items-center"
+          />
+        </span>
+        <h3 className="header">Country Explorer</h3>
+      </div>
+      <SearchBar />
     </div>
   );
 }
---
+++ country-explorer/src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { Search, X } from "lucide-react";
+
+export default function SearchBar({ searchCountry, setSearchCountry }) {
+  const hasText = searchCountry.trim().length > 0;
+  return (
+    <div>
+      <div className="glass mt-2 d-flex ">
+        <span>
+          <Search size={20} style={{ color: "orange" }} />
+        </span>
+        <input
+          type="text"
+          value={searchCountry}
+          onChange={(e) => setSearchCountry(e.target.value)}
+          placeholder="Search Country"
+          className="flex-grow-1 mx-2"
+        />
+        {hasText ? (
+          <button onClick={() => setSearchCountry("")}>
+            <X size={16} />
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
