457b0b7f152d83e69841d7988ba98f62b197163c
Maryam Mirzada <marimmirzada@gmail.com>
2026-02-14 21:49:36 +0430

    update the App.jsx

--- country-explorer/src/App.jsx
+++ country-explorer/src/App.jsx
@@ -1,8 +1,44 @@
 import "./App.css";
 import { Globe } from "lucide-react";
 import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [searchCountry, setSearchCountry] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [regain, setRegain] = useState("");
+
+  const trimedText = searchCountry.trim();
+
+  useEffect(() => {
+    if (!trimedText) {
+      setLoading(false);
+      setError(null);
+      setCountries("");
+      setRegain("");
+    }
+
+    try {
+      setLoading(true);
+      setError("");
+
+      async function fetchData() {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+
+        const data = await res.json();
+        return setCountries(data);
+      }
+    } catch {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [searchCountry]);
+  
   return (
     <div>
       <div className="body d-flex justify-content-center align-items-center pt-5 gap-2">
